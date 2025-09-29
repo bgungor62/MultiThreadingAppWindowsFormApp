23538503dd1ddb7fb5238634c88efc3ea81d0099
Baran Güngör <barangungor33@gmail.com>
2025-09-29 16:53:53 +0300

    MailObject classı oluşturuldu

---
+++ MultiThreadingApp/Models/MailObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreadingApp.Models
+{
+    public class MailObject
+    {
+        public string To { get; set; }
+        public string From { get; set; } = "testmail@gmail.com";
+        public MailProviderType MyProperty { get; set; }
+    }
+
+    public enum MailProviderType
+    {
+        None = 0,
+        Smtp = 1
+    }
+}
